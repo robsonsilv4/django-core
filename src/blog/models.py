8a726b672ec06ed3d6733449110453e677f85d93
robsonsilv4 <robsonsilv410@gmail.com>
2018-12-03 10:05:53 -0300

    Django Field Choices

--- src/blog/models.py
+++ src/blog/models.py
@@ -2,11 +2,20 @@ from django.db import models
 # from django.db.models import Model
 
 
+PUBLISH_CHOICES = (
+    ('draft', 'Draft'),
+    ('publish', 'Publish'),
+    ('private', 'Private'),
+)
+
+
 class PostModel(models.Model):
     id = models.BigAutoField(primary_key=True)
     # id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=240, verbose_name='Post Title')
     content = models.TextField(null=True, blank=True)
+    publish = models.CharField(
+        max_length=120, choices=PUBLISH_CHOICES, default='draft')
     active = models.BooleanField(default=True)  # null=True
 
     class Meta:
