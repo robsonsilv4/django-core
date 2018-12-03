8af6243aa14d7c1978c8f090cb00d70acc8c2307
robsonsilv4 <robsonsilv410@gmail.com>
2018-12-03 08:52:08 -0300

    App First Model Makemigrations

---
+++ src/blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+# from django.db.models import Model
+
+
+class PostModel(models.Model):
+    # id = models.AutoField(primary_key=True)
+    # id = models.IntegerField(primary_key=True)
+    pass
