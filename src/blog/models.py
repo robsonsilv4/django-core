68332ecc4d6e3a1b721944d36982fa7035c028fa
robsonsilv4 <robsonsilv410@gmail.com>
2018-12-03 09:09:53 -0300

    Boolean Field Null Default Values

--- src/blog/models.py
+++ src/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class PostModel(models.Model):
-    # id = models.AutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     # id = models.IntegerField(primary_key=True)
+    active = models.BooleanField(default=True)  # null=True
     pass
